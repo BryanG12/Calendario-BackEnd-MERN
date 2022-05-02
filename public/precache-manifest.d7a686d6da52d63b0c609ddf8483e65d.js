fa134c8f369b2c3a875e9b915818e67a36b599e1
BryanG12 <bryantizolg12@gmail.com>
2022-05-01 22:30:37 -0600

    v1 de calendar app

---
+++ public/precache-manifest.d7a686d6da52d63b0c609ddf8483e65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b2e7440902d7d2ac30",
+    "url": "/static/css/main.8710c076.chunk.css"
+  },
+  {
+    "revision": "35b2e7440902d7d2ac30",
+    "url": "/static/js/main.35b2e744.chunk.js"
+  },
+  {
+    "revision": "e3f9e3c0f5da52afe5b6",
+    "url": "/static/css/1.0df8db10.chunk.css"
+  },
+  {
+    "revision": "e3f9e3c0f5da52afe5b6",
+    "url": "/static/js/1.e3f9e3c0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb429846a503be9798d1582338e5eb5c",
+    "url": "/index.html"
+  }
+];
